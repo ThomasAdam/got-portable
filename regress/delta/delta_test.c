ac25a2928225d680fe46d1ff1bd086a2e7757365
Stefan Sperling <stsp@stsp.name>
2018-01-26 13:36:17 +0100

    add a skeleton for a delta test (empty)

---
+++ regress/delta/delta_test.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018 Stefan Sperling <stsp@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <sys/queue.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "got_error.h"
+
+#include "delta.h"
+
+#define RUN_TEST(expr, name) \
+	if (!(expr)) { printf("test %s failed\n", (name)); failure = 1; }
+
+static int
+delta_combine()
+{
+	return 1;
+}
+
+int
+main(int argc, const char *argv[])
+{
+	int failure = 0;
+
+	if (argc != 1) {
+		fprintf(stderr, "usage: delta_test [REPO_PATH]\n");
+		return 1;
+	}
+
+	RUN_TEST(delta_combine(), "delta_combine");
+
+	return failure ? 1 : 0;
+}
