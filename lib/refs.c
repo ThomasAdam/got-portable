38d42f420a1cb73c623fcda9776c1f679cb8dcb1
Stefan Sperling <stsp@stsp.name>
2018-02-12 22:57:02 +0100

    inline refs.h in refs.c

--- lib/refs.c
+++ lib/refs.c
@@ -34,7 +34,29 @@
 #include "delta.h"
 #include "zb.h"
 #include "object.h"
-#include "refs.h"
+
+/* A symbolic reference. */
+struct got_symref {
+	char *name;
+	char *ref;
+};
+
+/* A non-symbolic reference (there is no better designation). */
+struct got_ref {
+	char *name;
+	u_int8_t sha1[SHA1_DIGEST_LENGTH];
+};
+
+/* A reference which points to an arbitrary object. */
+struct got_reference {
+	unsigned int flags;
+#define GOT_REF_IS_SYMBOLIC	0x01
+
+	union {
+		struct got_ref ref;
+		struct got_symref symref;
+	} ref;
+};
 
 static const struct got_error *
 parse_symref(struct got_reference **ref, const char *name, const char *line)
