6c6d3ed32e74fe87db0f4bc0fbf16c620dc5301b
Stefan Sperling <stsp@stsp.name>
2018-04-02 11:34:26 +0200

    fix obsolete comment in struct got_repository definition

--- lib/got_lib_repository.h
+++ lib/got_lib_repository.h
@@ -24,6 +24,6 @@ struct got_repository {
 	/* The pack index cache speeds up search for packed objects. */
 	struct got_packidx_v2_hdr *packidx_cache[GOT_PACKIDX_CACHE_SIZE];
 
-	/* Open file handles, memory maps, and cached deltas for pack files. */
+	/* Open file handles for pack files. */
 	struct got_pack packs[GOT_PACK_CACHE_SIZE];
 };
