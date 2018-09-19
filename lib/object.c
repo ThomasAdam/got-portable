d9e7e1f62a236075d29f003698eaa271a930cc9b
Stefan Sperling <stsp@stsp.name>
2018-09-19 16:08:32 +0200

    match tree entries exactly, not by prefix, in find_entry_by_name()

--- lib/object.c
+++ lib/object.c
@@ -714,7 +714,8 @@ find_entry_by_name(struct got_tree_object *tree, const char *name, size_t len)
 	struct got_tree_entry *te;
 
 	SIMPLEQ_FOREACH(te, &tree->entries.head, entry) {
-		if (strncmp(te->name, name, len) == 0)
+		if (strlen(te->name) == len &&
+		    strncmp(te->name, name, len) == 0)
 			return te;
 	}
 	return NULL;
