36e2fb666259aadac2fb2c9399346aeeec2825b8
Stefan Sperling <stsp@stsp.name>
2019-01-04 17:35:53 +0100

    apply unveil(2) to 'got blame'

--- got/got.c
+++ got/got.c
@@ -985,8 +985,8 @@ cmd_blame(int argc, char *argv[])
 	int ch;
 
 #ifndef PROFILE
-	if (pledge("stdio rpath wpath cpath flock proc exec sendfd", NULL)
-	    == -1)
+	if (pledge("stdio rpath wpath cpath flock proc exec sendfd unveil",
+	    NULL) == -1)
 		err(1, "pledge");
 #endif
 
@@ -1026,6 +1026,10 @@ cmd_blame(int argc, char *argv[])
 			goto done;
 		}
 	}
+
+	error = apply_unveil(repo_path, NULL);
+	if (error)
+		goto done;
 
 	error = got_repo_open(&repo, repo_path);
 	if (error != NULL)
