5ea74b5a394cdadfb08905818a3bd053c5e06dfb
Stefan Sperling <stsp@stsp.name>
2019-02-11 14:20:09 +0100

    don't close input files in dump_delta_chain_to_file(); caller does it

--- lib/pack.c
+++ lib/pack.c
@@ -1134,10 +1134,6 @@ done:
 		if (len != accum_size)
 			err = got_ferror(outfile, GOT_ERR_IO);
 	}
-	if (base_file && fclose(base_file) != 0 && err == NULL)
-		err = got_error_from_errno();
-	if (accum_file && fclose(accum_file) != 0 && err == NULL)
-		err = got_error_from_errno();
 	rewind(outfile);
 	if (err == NULL)
 		*result_size = accum_size;
