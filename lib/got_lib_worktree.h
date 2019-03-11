7bcb2ecfc09de529ad7b240ac847222205ea9a4a
Stefan Sperling <stsp@stsp.name>
2019-03-11 20:24:21 +0100

    move worktree base refs into a subdirectory

--- lib/got_lib_worktree.h
+++ lib/got_lib_worktree.h
@@ -48,7 +48,7 @@ struct got_worktree {
 #define GOT_WORKTREE_FORMAT_VERSION	1
 #define GOT_WORKTREE_INVALID_COMMIT_ID	GOT_SHA1_STRING_ZERO
 
-#define GOT_WORKTREE_BASE_REF_PREFIX "got/worktree-base"
+#define GOT_WORKTREE_BASE_REF_PREFIX "got/worktree/base"
 
 const struct got_error *got_worktree_get_base_ref_name(char **,
     struct got_worktree *worktree);
