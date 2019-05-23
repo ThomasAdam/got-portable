6869fb7f545fc78d8a162ffca98601d0c69e21bb
Stefan Sperling <stsp@stsp.name>
2019-05-23 21:56:42 +0200

    remove unused include in got-read-pack

--- libexec/got-read-pack/got-read-pack.c
+++ libexec/got-read-pack/got-read-pack.c
@@ -36,7 +36,6 @@
 #include "got_object.h"
 
 #include "got_lib_delta.h"
-#include "got_lib_inflate.h"
 #include "got_lib_object.h"
 #include "got_lib_object_cache.h"
 #include "got_lib_object_parse.h"
