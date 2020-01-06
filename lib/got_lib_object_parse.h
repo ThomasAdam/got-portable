5aa813935b6489921083da5ed9459a1cfb8a03e3
Stefan Sperling <stsp@stsp.name>
2020-01-06 19:38:23 +0000

    add copyright year for files already touched in 2020

--- lib/got_lib_object_parse.h
+++ lib/got_lib_object_parse.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Stefan Sperling <stsp@openbsd.org>
+ * Copyright (c) 2018, 2019, 2020 Stefan Sperling <stsp@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
--- regress/cmdline/rebase.sh
+++ regress/cmdline/rebase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2019 Stefan Sperling <stsp@openbsd.org>
+# Copyright (c) 2019, 2020 Stefan Sperling <stsp@openbsd.org>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
