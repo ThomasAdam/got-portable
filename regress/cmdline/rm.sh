6b36edd809ff151ee866ec81cd007767d76cf778
Christian Weisgerber <naddy@mips.inka.de>
2020-10-03 19:15:22 +0000

    accommodate ls -l implementations that print "total 0" in an empty directory
    ok stsp

--- regress/cmdline/rm.sh
+++ regress/cmdline/rm.sh
@@ -239,7 +239,7 @@ test_rm_directory() {
 		return 1
 	fi
 
-	(cd $testroot/wt && ls -l > $testroot/stdout)
+	(cd $testroot/wt && ls -l | sed '/^total/d' > $testroot/stdout)
 
 	echo -n '' > $testroot/stdout.expected
 
@@ -251,7 +251,7 @@ test_rm_directory() {
 		return 1
 	fi
 
-	(cd $testroot/wt && ls -l > $testroot/stdout)
+	(cd $testroot/wt && ls -l | sed '/^total/d' > $testroot/stdout)
 
 	echo -n '' > $testroot/stdout.expected
 
