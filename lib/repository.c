2252c019b5ec7ee342e8ebf2854ec8d83182b3ee
Stefan Sperling <stsp@stsp.name>
2021-07-03 22:55:27 +0000

    Update a code comment regarding support of the preciousObjects extension.

--- lib/repository.c
+++ lib/repository.c
@@ -610,7 +610,7 @@ read_gotconfig(struct got_repository *repo)
 /* Supported repository format extensions. */
 static const char *repo_extensions[] = {
 	"noop",			/* Got supports repository format version 1. */
-	"preciousObjects",	/* Got has no garbage collection yet. */
+	"preciousObjects",	/* Supported by gotadmin cleanup. */
 	"worktreeConfig",	/* Got does not care about Git work trees. */
 };
 
