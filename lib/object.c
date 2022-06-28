acb9e3ea2ed831226f75eb7606ecd06a0711b131
Stefan Sperling <stsp@stsp.name>
2022-06-28 18:19:03 +0000

    pass the correct file to fdopen(); ok tracey

--- lib/object.c
+++ lib/object.c
@@ -1426,7 +1426,7 @@ open_blob(struct got_blob_object **blob, struct got_repository *repo,
 			goto done;
 		}
 
-		(*blob)->f = fdopen(outfd, "rb");
+		(*blob)->f = fdopen(dfd, "rb");
 		if ((*blob)->f == NULL) {
 			err = got_error_from_errno("fdopen");
 			close(dfd);
