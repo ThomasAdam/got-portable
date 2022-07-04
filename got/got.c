ffcfe569470b9a24db65cf13a33fb059d17752a2
Stefan Sperling <stsp@stsp.name>
2022-07-04 11:05:06 +0000

    got tag -V is like tag -l with verification, fix option-conflicts accordingly

--- got/got.c
+++ got/got.c
@@ -7367,28 +7367,22 @@ cmd_tag(int argc, char *argv[])
 		if (commit_id_arg != NULL)
 			errx(1,
 			    "-c option can only be used when creating a tag");
-		if (tagmsg)
-			option_conflict('l', 'm');
-		if (signer_id)
-			option_conflict('l', 's');
-		if (verify_tags)
-			option_conflict('l', 'V');
+		if (tagmsg) {
+			if (do_list)
+				option_conflict('l', 'm');
+			else
+				option_conflict('V', 'm');
+		}
+		if (signer_id) {
+			if (do_list)
+				option_conflict('l', 's');
+			else
+				option_conflict('V', 's');
+		}
 		if (argc > 1)
 			usage_tag();
 	} else if (argc != 1)
 		usage_tag();
-
-	if (verify_tags) {
-		if (commit_id_arg != NULL)
-			errx(1,
-			    "-c option can only be used when creating a tag");
-		if (tagmsg)
-			option_conflict('V', 'm');
-		if (signer_id)
-			option_conflict('V', 's');
-		if (do_list)
-			option_conflict('V', 'l');
-	}
 
 	if (argc == 1)
 		tag_name = argv[0];
