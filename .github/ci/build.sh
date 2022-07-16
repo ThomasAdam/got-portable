25443dd1e0f215944df18fb7445d253be205bd58
Thomas Adam <thomas@xteddy.org>
2022-07-16 15:34:37 +0100

    Revert "portable: ci: macos: add libressl"
    Remove dependency on libretls as TLS isn't going to be included in
    gotwebd any time soon.
    
    This reverts commit af7f5027866eea7b4299c2319f83d0eb18a395e6.

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -2,10 +2,10 @@
 
 [ "$CIRRUS_OS" = "darwin" ] && {
 	export PATH="/usr/local/opt/bison/bin:$PATH"
-	export LDFLAGS="-L/usr/local/opt/ncurses/lib -L/usr/local/opt/openssl@3/lib -L/usr/local/opt/libressl/lib"
-	export CPPFLAGS="-I/usr/local/opt/ncurses/include -I/usr/local/opt/openssl@3/include -I/usr/local/opt/libressl/include"
+	export LDFLAGS="-L/usr/local/opt/ncurses/lib -L/usr/local/opt/openssl@3/lib"
+	export CPPFLAGS="-I/usr/local/opt/ncurses/include -I/usr/local/opt/openssl@3/include"
 	export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/openssl@3/lib/pkgconfig -I/usr/local/opt/libressl/lib/pkgconfig"
+	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/openssl@3/lib/pkgconfig"
 }
 
 ./autogen.sh || exit 1
