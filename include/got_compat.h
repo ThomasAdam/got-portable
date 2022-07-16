19467d598676c21cf908e3ee8f60a6e04306cede
Thomas Adam <thomas@xteddy.org>
2022-07-16 12:54:26 +0100

    portable: honour sys/queue.h directly
    Don't assert compat/queue.h in favour of what's on the system.  When
    -portable was first being developed, there were frequent clashes between
    libbsd, and the check for sys/queue.h got lost in that noise.
    
    Noticed by stsp@

--- include/got_compat.h
+++ include/got_compat.h
@@ -114,10 +114,13 @@ void uuid_to_string(uuid_t *, char **, uint32_t *);
 #include <inttypes.h>
 #endif
 
-#ifdef HAVE_QUEUE_H
-#include <sys/queue.h>
-#else
+/* Only include sys/queue.h if it's not found on the system (libbsd provides
+ * a copy of this).
+ */
+#if !defined(HAVE_QUEUE_H) && !defined(HAVE_LIBBSD)
 #include "compat/queue.h"
+#else
+#include <sys/queue.h>
 #endif
 
 #ifdef HAVE_TREE_H
