598139dd22c7c7517a6542803c64e88acd78b162
Thomas Adam <thomas@xteddy.org>
2022-07-21 15:18:07 +0100

    portable: add repo version to main string
    When compiling got-portable from source, outside of an official release
    tarball, it's often helpful to know which commit the binary was built
    from, especially for diagnostic purposes.
    
    This adds an extra hook to AC_INIT to derive whether -portable is being
    built in-tree or via a release tarball.

---
+++ util/got-portable-ver.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# got-portable-ver: emits the version of b-em which is building.
+#		    If this is a release build, then the tag name is chomped
+#		    to remove extraneous git information.
+#
+#		    If it's a developer build, it's left as-is.
+#
+# Intended to be called from configure.ac (via autogen.sh)
+GOT_PORTABLE_VER=0.75
+
+[ -d ".git" ] || { echo "$B_EM_VERSION" ; exit ; }
+
+git describe --always --dirty 2>/dev/null || \
+	echo "$GOT_PORTABLE_VER"
