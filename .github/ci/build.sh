ec879696141a2ca062685a3ca13ec7f5ddf29736
Thomas Adam <thomas@xteddy.org>
2022-07-21 16:04:37 +0100

    Macos

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-[ "$CIRRUS_OS" = "darwin" ] && {
-	export PATH="/usr/local/opt/bison/bin:$PATH"
-	export LDFLAGS="-L/usr/local/opt/ncurses/lib -L/usr/local/opt/openssl@3/lib"
-	export CPPFLAGS="-I/usr/local/opt/ncurses/include -I/usr/local/opt/openssl@3/include"
-	export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
-	export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/local/opt/openssl@3/lib/pkgconfig"
-}
-
 ./autogen.sh || exit 1
 ./configure || exit 1
 exec make
