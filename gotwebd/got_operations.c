ThomasAdam/got-portable

commit 7ecc4542fefb0ad0b7babe055da2f9ac447a906b

gotwebd: switch some HTML IDs to classes
HTML IDs are meant to be unique across the page.  While here fix two
small errors in the generated HTML (one missing closing div and one
extra.)

ok tracey@

gotwebd/files/htdocs/gotwebd/gotweb.css
@@ -60,12 +60,12 @@ body {
 #logo {
 	height: 50px;
 }
-#refs_str {
+.refs_str {
 	background-color: #243647;
 	color: #ffffff;
 	font-style: italic;
 }
-#dotted_line {
+.dotted_line {
 	clear: left;
 	float: left;
 	width: 100%;
@@ -106,12 +106,6 @@ body {
 	color: #ffffff;
 	text-decoration: none;
 }
-#search {
-	float: right;
-	padding-right: 10px;
-	padding-top: 5px;
-	padding-bottom: 5px;
-}
 #got_link {
 	float: left;
 	padding-bottom: 10px;
@@ -142,13 +136,13 @@ body {
 	text-align: right;
 	overflow: hidden;
 }
-#navs_wrapper {
+.navs_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 	background-color: #ced7e0;
 }
-#navs {
+.navs {
 	padding-left: 10px;
 	padding-top: 2px;
 	padding-bottom: 2px;
@@ -225,7 +219,7 @@ body {
 	white-space: pre-wrap;
 }
 
-#header_commit_title {
+.header_commit_title {
 	clear: left;
 	float: left;
 	width: 6.5em;
@@ -233,7 +227,7 @@ body {
 	padding-top: 2px;
 	padding-bottom: 2px;
 }
-#header_commit {
+.header_commit {
 	float: left;
 	width: 72%;
 	padding-top: 2px;
@@ -253,7 +247,7 @@ body {
 	padding-top: 2px;
 	padding-bottom: 2px;
 }
-#header_author_title {
+.header_author_title {
 	clear: left;
 	float: left;
 	width: 6.5em;
@@ -261,13 +255,13 @@ body {
 	padding-top: 2px;
 	padding-bottom: 2px;
 }
-#header_author {
+.header_author {
 	float: left;
 	width: 72%;
 	padding-top: 2px;
 	padding-bottom: 2px;
 }
-#header_committer_title {
+.header_age_title {
 	clear: left;
 	float: left;
 	width: 6.5em;
@@ -275,21 +269,7 @@ body {
 	padding-top: 2px;
 	padding-bottom: 2px;
 }
-#header_committer {
-	float: left;
-	width: 72%;
-	padding-top: 2px;
-	padding-bottom: 2px;
-}
-#header_age_title {
-	clear: left;
-	float: left;
-	width: 6.5em;
-	padding-left: 10px;
-	padding-top: 2px;
-	padding-bottom: 2px;
-}
-#header_age {
+.header_age {
 	float: left;
 	width: 72%;
 	padding-top: 2px;
@@ -351,7 +331,7 @@ body {
 	float: left;
 	width: 100%;
 }
-#briefs_age {
+.briefs_age {
 	padding-left: 10px;
 	padding-top: 5px;
 	padding-bottom: 5px;
@@ -359,7 +339,7 @@ body {
 	width: 7.5em;
 	overflow: auto;
 }
-#briefs_author {
+.briefs_author {
 	float: left;
 	padding-top: 5px;
 	padding-bottom: 5px;
@@ -367,7 +347,7 @@ body {
 	font-style: italic;
 	overflow: auto;
 }
-#briefs_log {
+.briefs_log {
 	float: left;
 	padding-left: 10px;
 	padding-right: 10px;
@@ -393,7 +373,7 @@ body {
 	float: left;
 	width: 100%;
 }
-#tag_age {
+.tag_age {
 	padding-left: 10px;
 	padding-top: 5px;
 	padding-bottom: 5px;
@@ -401,7 +381,7 @@ body {
 	width: 7.5em;
 	overflow: auto;
 }
-#tags_log {
+.tag_log {
 	float: left;
 	padding-left: 10px;
 	padding-right: 10px;
@@ -423,7 +403,7 @@ body {
 	padding-bottom: 2px;
 	width: 80%;
 }
-#tag {
+.tag {
 	float: left;
 	width: 8.5em;
 	font-style: italic;
@@ -465,48 +445,40 @@ body {
 	padding: 10px;
 	overflow: hidden;
 }
-#index_wrapper {
+.index_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 }
-#index_project {
+.index_project {
 	float: left;
 	width: 20%;
 	padding: 10px;
 	overflow: hidden;
 }
-#index_project_description {
+.index_project_description {
 	float: left;
 	width: 30%;
 	padding: 10px;
 	overflow: auto;
 }
-#index_project_owner {
+.index_project_owner {
 	float: left;
 	width: 12%;
 	padding: 10px;
 	overflow: hidden;
 }
-#index_project_age {
+.index_project_age {
 	float: left;
 	width: 14%;
 	padding: 10px;
 	overflow: visible;
 }
-#index_project a {
+.index_project a {
 	color: #444444;
 	text-decoration: none;
 }
-#index_project a:hover {
-	color: SteelBlue;
-	text-decoration: none;
-}
-#index_project_navs a {
-	color: #444444;
-	text-decoration: none;
-}
-#index_project_navs a:hover {
+.index_project a:hover {
 	color: SteelBlue;
 	text-decoration: none;
 }
@@ -527,44 +499,40 @@ body {
 	text-decoration: none;
 }
 
-#commits_title_wrapper {
+.commits_title_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 	background-color: LightSlateGray;
 	color: #ffffff;
 }
-#commits_title {
+.commits_title {
 	padding-left: 10px;
 	padding-top: 5px;
 	padding-bottom: 5px;
 }
-#commits_content {
+.commits_content {
 	clear: left;
 	float: left;
 	width: 100%;
 }
-#commits_header_wrapper {
+.commits_header_wrapper {
 	float: left;
 	background-color: #f5fcfb;
 	width: 100%;
 }
-#commits_header {
+.commits_header {
 	float: left;
 	padding-top: 5px;
 	padding-bottom: 2px;
 	width: 80%;
 }
-#commit {
+.commit {
 	clear: left;
 	float: left;
 	padding-left: 20px;
 	padding-bottom: 20px;
 	white-space: pre-wrap;
-}
-#commits_line {
-	clear: left;
-	float: left;
 }
 
 #blame_title_wrapper {
@@ -605,32 +573,32 @@ body {
 	white-space: pre;
 	overflow: auto;
 }
-#blame_wrapper {
+.blame_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 }
-#blame_number {
+.blame_number {
 	float: left;
 	width: 6em;
 	overflow: hidden;
 }
-#blame_hash {
+.blame_hash {
 	float: left;
 	width: 6em;
 	overflow: auto;
 }
-#blame_date {
+.blame_date {
 	float: left;
 	width: 7em;
 	overflow: auto;
 }
-#blame_author {
+.blame_author {
 	float: left;
 	width: 6em;
 	overflow: hidden;
 }
-#blame_code {
+.blame_code {
 	float:left;
 	width: 50%;
 	overflow: visible;
@@ -674,26 +642,21 @@ body {
 	margin-bottom: 20px;
 	font-family: monospace;
 }
-#tree_wrapper {
+.tree_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 }
-#tree_line {
+.tree_line {
 	clear: left;
 	float: left;
 	width: 20em;
 	padding: 1px;
 }
-#tree_line_blank {
+.tree_line_blank {
 	float: left;
 	padding: 1px;
 	width: 9.5em;
-}
-#tree_line_navs {
-	float: left;
-	text-align: right;
-	padding: 1px;
 }
 
 #diff_title_wrapper {
@@ -733,7 +696,7 @@ body {
 	font-family: monospace;
 	white-space: pre;
 }
-#diff_line {
+.diff_line {
 	clear: left;
 	float: left;
 }
@@ -763,12 +726,12 @@ body {
 	width: 100%;
 }
 
-#branches_wrapper {
+.branches_wrapper {
 	clear: left;
 	float: left;
 	width: 100%;
 }
-#branches_age {
+.branches_age {
 	padding-left: 10px;
 	padding-top: 5px;
 	padding-bottom: 5px;
@@ -776,7 +739,7 @@ body {
 	width: 7.5em;
 	overflow: auto;
 }
-#branches_space {
+.branches_space {
 	padding-left: 10px;
 	padding-top: 5px;
 	padding-bottom: 5px;
@@ -784,7 +747,7 @@ body {
 	width: 8.5em;
 	overflow: auto;
 }
-#branch {
+.branch {
 	float: left;
 	padding-right: 10px;
 	padding-top: 5px;

gotwebd/got_operations.c
@@ -947,10 +947,10 @@ got_output_repo_tree(struct request *c)
 			}
 
 			if (fcgi_gen_response(c,
-			    "<div id='tree_wrapper'>\n") == -1)
+			    "<div class='tree_wrapper'>\n") == -1)
 			goto done;
 
-			if (fcgi_gen_response(c, "<div id='tree_line' "
+			if (fcgi_gen_response(c, "<div class='tree_line' "
 			    "class='") == -1)
 				goto done;
 			if (fcgi_gen_response(c, class) == -1)
@@ -989,7 +989,7 @@ got_output_repo_tree(struct request *c)
 			if (fcgi_gen_response(c, "</div>\n") == -1)
 				goto done;
 
-			if (fcgi_gen_response(c, "<div id='tree_line_blank' "
+			if (fcgi_gen_response(c, "<div class='tree_line_blank' "
 			    "class='") == -1)
 				goto done;
 			if (fcgi_gen_response(c, class) == -1)
@@ -1013,9 +1013,9 @@ got_output_repo_tree(struct request *c)
 			}
 
 			if (fcgi_gen_response(c,
-			    "<div id='tree_wrapper'>\n") == -1)
+			    "<div class='tree_wrapper'>\n") == -1)
 				goto done;
-			if (fcgi_gen_response(c, "<div id='tree_line' "
+			if (fcgi_gen_response(c, "<div class='tree_line' "
 			    "class='") == -1)
 				goto done;
 			if (fcgi_gen_response(c, class) == -1)
@@ -1066,7 +1066,7 @@ got_output_repo_tree(struct request *c)
 			if (fcgi_gen_response(c, "</div>\n") == -1)
 				goto done;
 
-			if (fcgi_gen_response(c, "<div id='tree_line_blank' "
+			if (fcgi_gen_response(c, "<div class='tree_line_blank' "
 			    "class='") == -1)
 				goto done;
 			if (fcgi_gen_response(c, class) == -1)
@@ -1400,9 +1400,9 @@ got_gotweb_blame_cb(void *arg, int nlines, int lineno,
 		if (nl)
 			*nl = '\0';
 
-		if (fcgi_gen_response(c, "<div id='blame_wrapper'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_wrapper'>") == -1)
 			goto done;
-		if (fcgi_gen_response(c, "<div id='blame_number'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_number'>") == -1)
 			goto done;
 		if (snprintf(out_buff, strlen(out_buff), "%.*d", a->nlines_prec,
 		    a->lineno_cur) < 0)
@@ -1412,7 +1412,7 @@ got_gotweb_blame_cb(void *arg, int nlines, int lineno,
 		if (fcgi_gen_response(c, "</div>") == -1)
 			goto done;
 
-		if (fcgi_gen_response(c, "<div id='blame_hash'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_hash'>") == -1)
 			goto done;
 
 		if (fcgi_gen_response(c, "<a href='?index_page=") == -1)
@@ -1436,21 +1436,21 @@ got_gotweb_blame_cb(void *arg, int nlines, int lineno,
 		if (fcgi_gen_response(c, "</a></div>") == -1)
 			goto done;
 
-		if (fcgi_gen_response(c, "<div id='blame_date'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_date'>") == -1)
 			goto done;
 		if (fcgi_gen_response(c, bline->datebuf) == -1)
 			goto done;
 		if (fcgi_gen_response(c, "</div>") == -1)
 			goto done;
 
-		if (fcgi_gen_response(c, "<div id='blame_author'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_author'>") == -1)
 			goto done;
 		if (fcgi_gen_response(c, committer) == -1)
 			goto done;
 		if (fcgi_gen_response(c, "</div>") == -1)
 			goto done;
 
-		if (fcgi_gen_response(c, "<div id='blame_code'>") == -1)
+		if (fcgi_gen_response(c, "<div class='blame_code'>") == -1)
 			goto done;
 		err = gotweb_escape_html(&eline, line);
 		if (err)
@@ -1806,7 +1806,7 @@ got_output_repo_diff(struct request *c)
 				goto done;
 			}
 		}
-		if (fcgi_gen_response(c, "<div id='diff_line' class='") == -1)
+		if (fcgi_gen_response(c, "<div class='diff_line' class='") == -1)
 			goto done;
 		if (fcgi_gen_response(c, color ? color : "") == -1)
 			goto done;

commit bf80b15220f51490025e916633cdd70816113604

gotwebd: fix alternate colors in tree entries
it was broken in 7ecc454 because now we're emitting tags with multiple
`class' attributes (by mistake) and only the first one is used.
However, instead of fixing the HTML `class' attribute, use a CSS
selector to alternate the background colors.

ok tracey@

gotwebd/files/htdocs/gotwebd/gotweb.css
@@ -50,13 +50,6 @@ body {
 	color: blue;
 }
 
-.back_white {
-	background-color: #ffffff;
-}
-.back_lightgray {
-	background-color: #d8f3ef;
-}
-
 #logo {
 	height: 50px;
 }
@@ -646,6 +639,10 @@ body {
 	clear: left;
 	float: left;
 	width: 100%;
+	background-color: #d8f3ef;
+}
+.tree_wrapper:nth-child(even) {
+	background-color: #ffffff;
 }
 .tree_line {
 	clear: left;

gotwebd/got_operations.c
@@ -831,8 +831,8 @@ got_output_repo_tree(struct request *c)
 	struct repo_dir *repo_dir = t->repo_dir;
 	char *id_str = NULL;
 	char *path = NULL, *in_repo_path = NULL, *build_folder = NULL;
-	char *modestr = NULL, *name = NULL, *class = NULL;
-	int nentries, i, class_flip = 0;
+	char *modestr = NULL, *name = NULL;
+	int nentries, i;
 
 	TAILQ_INIT(&refs);
 
@@ -917,22 +917,6 @@ got_output_repo_tree(struct request *c)
 			}
 		}
 
-		if (class_flip == 0) {
-			class = strdup("back_lightgray");
-			if (class == NULL) {
-				error = got_error_from_errno("strdup");
-				goto done;
-			}
-			class_flip = 1;
-		} else {
-			class = strdup("back_white");
-			if (class == NULL) {
-				error = got_error_from_errno("strdup");
-				goto done;
-			}
-			class_flip = 0;
-		}
-
 		name = strdup(got_tree_entry_get_name(te));
 		if (name == NULL) {
 			error = got_error_from_errno("strdup");
@@ -950,12 +934,7 @@ got_output_repo_tree(struct request *c)
 			    "<div class='tree_wrapper'>\n") == -1)
 			goto done;
 
-			if (fcgi_gen_response(c, "<div class='tree_line' "
-			    "class='") == -1)
-				goto done;
-			if (fcgi_gen_response(c, class) == -1)
-				goto done;
-			if (fcgi_gen_response(c, "'>") == -1)
+			if (fcgi_gen_response(c, "<div class='tree_line'>") == -1)
 				goto done;
 
 			if (fcgi_gen_response(c, "<a class='diff_directory' "
@@ -989,12 +968,7 @@ got_output_repo_tree(struct request *c)
 			if (fcgi_gen_response(c, "</div>\n") == -1)
 				goto done;
 
-			if (fcgi_gen_response(c, "<div class='tree_line_blank' "
-			    "class='") == -1)
-				goto done;
-			if (fcgi_gen_response(c, class) == -1)
-				goto done;
-			if (fcgi_gen_response(c, "'>") == -1)
+			if (fcgi_gen_response(c, "<div class='tree_line_blank'>") == -1)
 				goto done;
 			if (fcgi_gen_response(c, "&nbsp;") == -1)
 				goto done;
@@ -1015,12 +989,7 @@ got_output_repo_tree(struct request *c)
 			if (fcgi_gen_response(c,
 			    "<div class='tree_wrapper'>\n") == -1)
 				goto done;
-			if (fcgi_gen_response(c, "<div class='tree_line' "
-			    "class='") == -1)
-				goto done;
-			if (fcgi_gen_response(c, class) == -1)
-				goto done;
-			if (fcgi_gen_response(c, "'>") == -1)
+			if (fcgi_gen_response(c, "<div class='tree_line'>") == -1)
 				goto done;
 
 			if (fcgi_gen_response(c,
@@ -1066,12 +1035,7 @@ got_output_repo_tree(struct request *c)
 			if (fcgi_gen_response(c, "</div>\n") == -1)
 				goto done;
 
-			if (fcgi_gen_response(c, "<div class='tree_line_blank' "
-			    "class='") == -1)
-				goto done;
-			if (fcgi_gen_response(c, class) == -1)
-				goto done;
-			if (fcgi_gen_response(c, "'>") == -1)
+			if (fcgi_gen_response(c, "<div class='tree_line_blank'>") == -1)
 				goto done;
 
 			if (fcgi_gen_response(c,
@@ -1166,8 +1130,6 @@ got_output_repo_tree(struct request *c)
 		name = NULL;
 		free(modestr);
 		modestr = NULL;
-		free(class);
-		class = NULL;
 	}
 done:
 	free(id_str);
@@ -1175,7 +1137,6 @@ done:
 	free(modestr);
 	free(path);
 	free(name);
-	free(class);
 	got_ref_list_free(&refs);
 	if (commit)
 		got_object_commit_close(commit);