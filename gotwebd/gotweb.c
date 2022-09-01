ThomasAdam/got-portable

commit 625e5896fc9ecf87ccfc92ad2a65cd3be58f73c0

gotwebd: free all allocated fields of repo_tag
got_get_repo_tags needs some tweaking to how tag_commit is allocated
too.

ok stsp@

gotwebd/gotweb.c
@@ -538,8 +538,10 @@ void
 gotweb_free_repo_tag(struct repo_tag *rt)
 {
 	if (rt != NULL) {
-		free(rt->commit_msg);
 		free(rt->commit_id);
+		free(rt->tag_name);
+		free(rt->tag_commit);
+		free(rt->commit_msg);
 		free(rt->tagger);
 	}
 	free(rt);

gotwebd/got_operations.c
@@ -573,6 +573,7 @@ got_get_repo_tags(struct request *c, int limit)
 	struct got_tag_object *tag = NULL;
 	struct repo_tag *rt = NULL, *trt = NULL;
 	char *in_repo_path = NULL, *repo_path = NULL, *id_str = NULL;
+	char *tag_commit = NULL, *tag_commit0 = NULL;
 	char *commit_msg = NULL, *commit_msg0 = NULL;
 	int chk_next = 0, chk_multi = 1, commit_found = 0, c_cnt = 0;
 
@@ -724,23 +725,30 @@ got_get_repo_tags(struct request *c, int limit)
 		}
 
 		if (commit) {
-			error = got_object_commit_get_logmsg(&new_repo_tag->
-			    tag_commit, commit);
+			error = got_object_commit_get_logmsg(&tag_commit0,
+			    commit);
 			if (error)
-				goto done;
+				goto err;
 			got_object_commit_close(commit);
 			commit = NULL;
 		} else {
-			new_repo_tag->tag_commit =
-			    strdup(got_object_tag_get_message(tag));
-			if (new_repo_tag->tag_commit == NULL) {
+			tag_commit0 = strdup(got_object_tag_get_message(tag));
+			if (tag_commit0 == NULL) {
 				error = got_error_from_errno("strdup");
-				goto done;
+				goto err;
 			}
 		}
 
-		while (*new_repo_tag->tag_commit == '\n')
-			new_repo_tag->tag_commit++;
+		tag_commit = tag_commit0;
+		while (*tag_commit == '\n')
+			tag_commit++;
+		new_repo_tag->tag_commit = strdup(tag_commit);
+		if (new_repo_tag->tag_commit == NULL) {
+			error = got_error_from_errno("strdup");
+			free(tag_commit0);
+			goto err;
+		}
+		free(tag_commit0);
 
 		if (qs->action != SUMMARY && qs->action != TAGS) {
 			commit_msg = commit_msg0;

commit 5fba075071d3c6894ede739a7fc9b791a7f392f7

fix regression in gotweb_free_repo_tag(): free(...): modified chunk-pointer
problem introduced in commit 625e5896fc9ecf87ccfc92ad2a65cd3be58f73c0

gotwebd/gotweb.c
@@ -1693,9 +1693,10 @@ gotweb_render_tag(struct request *c)
 	if (error)
 		goto done;
 
-	if (strncmp(rt->tag_name, "refs/", 5) == 0)
-		rt->tag_name += 5;
-	error = gotweb_escape_html(&tagname, rt->tag_name);
+	tagname = rt->tag_name;
+	if (strncmp(tagname, "refs/", 5) == 0)
+		tagname += 5;
+	error = gotweb_escape_html(&tagname, tagname);
 	if (error)
 		goto done;
 
@@ -1780,9 +1781,10 @@ gotweb_render_tags(struct request *c)
 		if (error)
 			goto done;
 
-		if (strncmp(rt->tag_name, "refs/tags/", 10) == 0)
-			rt->tag_name += 10;
-		error = gotweb_escape_html(&tagname, rt->tag_name);
+		tagname = rt->tag_name;
+		if (strncmp(tagname, "refs/tags/", 10) == 0)
+			tagname += 10;
+		error = gotweb_escape_html(&tagname, tagname);
 		if (error)
 			goto done;
 