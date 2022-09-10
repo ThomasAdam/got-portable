05345ace5375ae9a980633912315c289c71c9163
Thomas Adam <thomas@xteddy.org>
2022-09-10 17:07:49 +0100

    portable: use a release variable for tarballs
    When creating a release tarball, use a GOT_RELEASE variable to determine
    the version of the release.

--- util/got-portable-ver.sh
+++ util/got-portable-ver.sh
@@ -7,9 +7,10 @@
 #		    If it's a developer build, it's left as-is.
 #
 # Intended to be called from configure.ac (via autogen.sh)
+GOT_RELEASE=no
 GOT_PORTABLE_VER=0.75
 
-[ -d ".git" ] || { echo "$GOT_PORTABLE_VER" ; exit ; }
+[ -d ".git" -a "$GOT_RELEASE" = "no" ] || { echo "$GOT_PORTABLE_VER" ; exit ; }
 
 git describe --always --dirty 2>/dev/null || \
 	echo "$GOT_PORTABLE_VER"
