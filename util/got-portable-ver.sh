6dbbfded937c923383970d564ff69f8c7eda2be4
Thomas Adam <thomas@xteddy.org>
2022-11-03 22:32:00 +0000

    portable: set next version

--- util/got-portable-ver.sh
+++ util/got-portable-ver.sh
@@ -7,8 +7,8 @@
 #		    If it's a developer build, it's left as-is.
 #
 # Intended to be called from configure.ac (via autogen.sh)
-GOT_RELEASE=yes
-GOT_PORTABLE_VER=0.78
+GOT_RELEASE=no
+GOT_PORTABLE_VER=0.79
 
 [ -d ".git" -a "$GOT_RELEASE" = "no" ] || { echo "$GOT_PORTABLE_VER" ; exit ; }
 
