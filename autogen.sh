f600ad7605fa8b629252e6b11daae241ebe0d135
Thomas Adam <thomas@xteddy.org>
2023-01-06 10:10:17 +0000

    portable: OpenBSD support
    Despite -portable existing to make the codebase compile on platforms
    other than OpenBSD, there's nothing wrong with making -portable still
    compile on that platform as well.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ "x$(uname)" = "xOpenBSD" ]; then
+	[ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.16
+	[ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.69
+fi
+
+
 die()
 {
     echo "$1" >&2
