71d196c9e2bffa978977785cd8859579e78bac24
Christian Weisgerber <naddy@mips.inka.de>
2023-01-18 16:14:48 +0000

    regress: replace typeset with printf for shell portability
    The typeset shell builtin is a ksh(1) feature.  It is not available
    in a pure POSIX sh(1).  Use printf(1) to portably perform the same
    string truncation.
    
    tweak and ok op jamsek

--- regress/cmdline/diff.sh
+++ regress/cmdline/diff.sh
@@ -1762,10 +1762,8 @@ EOF
 		return 1
 	fi
 
-	typeset -L10 short_alpha_id
-	typeset -L10 short_alpha_new_id
-	short_alpha_id=$alpha_blobid
-	short_alpha_new_id=$alpha_new_blobid
+	short_alpha_id=$(printf '%.10s' $alpha_blobid)
+	short_alpha_new_id=$(printf '%.10s' $alpha_new_blobid)
 	cat <<EOF >$testroot/stdout.expected
 diffstat $alpha_blobid $alpha_new_blobid
  M  $short_alpha_id -> $short_alpha_new_id  |  1+  1-
