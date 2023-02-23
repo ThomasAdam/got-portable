b221ffcbb1a5002260d0a678b8734656f6e18670
Thomas Adam <thomas@xteddy.org>
2023-02-23 22:01:48 +0000

    portable: remove endian.h
    This is included portably.

--- lib/pack.c
+++ lib/pack.c
@@ -29,7 +29,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include <endian.h>
 #include <unistd.h>
 #include <zlib.h>
 
--- lib/pack_index.c
+++ lib/pack_index.c
@@ -31,7 +31,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <endian.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <zlib.h>
