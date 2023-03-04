7161c4dc69ceb7852360acd62d0cba0da0d5afb4
Josiah Frentsos <jfrent@tilde.team>
2023-03-04 08:55:29 +0000

    gotd: Fix more double process names
    Patch by Josiah Frentsos, thanks!

--- gotd/repo_write.c
+++ gotd/repo_write.c
@@ -1511,7 +1511,7 @@ repo_write_shutdown(void)
 	struct repo_write_client *client = &repo_write_client;
 	struct gotd_ref_update *ref_update;
 
-	log_debug("%s: shutting down", repo_write.title);
+	log_debug("shutting down");
 
 	while (!STAILQ_EMPTY(&client->ref_updates)) {
 		ref_update = STAILQ_FIRST(&client->ref_updates);
