1c2f8577ce1acf4b16371f61fe9a4cc2e53b7869
Thomas Adam <thomas@xteddy.org>
2023-03-10 22:46:56 +0000

    portable: autogen: remove AUTO{MAKE,CONF}_VERSION
    When running the bootstrapping shell script autogen.sh, no longer set
    the AUTO{MAKE,CONF}_VERSION env vars.
    
    Although these were guarded and only set to default values on OpenBSD if
    they weren't already set, these should ideally be set by individual
    users on OpenBSD themselves.  Different versions of OpenBSD could well
    have different versions.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-if [ "x$(uname)" = "xOpenBSD" ]; then
-	[ -z "$AUTOMAKE_VERSION" ] && export AUTOMAKE_VERSION=1.16
-	[ -z "$AUTOCONF_VERSION" ] && export AUTOCONF_VERSION=2.69
-fi
-
-
 die()
 {
     echo "$1" >&2
