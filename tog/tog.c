8d212112582e612c39aa399858bc0b10c74782d0
Mark Jamsek <mark@jamsek.dev>
2023-04-16 07:33:14 +0000

    tog: blame regress and new test framework wait instruction
    Basic blame view test required adding the WAIT_FOR_UI instruction to delay
    screen dumps till the ui has been completely rendered. This will also apply
    to future test cases such as limited log views and further blame test cases.
    
    ok stsp@

--- tog/tog.c
+++ tog/tog.c
@@ -617,6 +617,7 @@ struct tog_io {
 	FILE	*cin;
 	FILE	*cout;
 	FILE	*f;
+	int	 wait_for_ui;
 } tog_io;
 static int using_mock_io;
 
@@ -1629,6 +1630,11 @@ tog_read_script_key(FILE *script, int *ch, int *done)
 	char			*line = NULL;
 	size_t			 linesz = 0;
 
+	*ch = -1;
+
+	if (tog_io.wait_for_ui)
+		goto done;
+
 	if (getline(&line, &linesz, script) == -1) {
 		if (feof(script)) {
 			*done = 1;
@@ -1637,7 +1643,10 @@ tog_read_script_key(FILE *script, int *ch, int *done)
 			err = got_ferror(script, GOT_ERR_IO);
 			goto done;
 		}
-	} else if (strncasecmp(line, "KEY_ENTER", 9) == 0)
+	}
+	if (strncasecmp(line, "WAIT_FOR_UI", 11) == 0)
+		tog_io.wait_for_ui = 1;
+	else if (strncasecmp(line, "KEY_ENTER", 9) == 0)
 		*ch = KEY_ENTER;
 	else if (strncasecmp(line, "KEY_RIGHT", 9) == 0)
 		*ch = KEY_RIGHT;
@@ -6111,6 +6120,11 @@ draw_blame(struct tog_view *view)
 	s->last_displayed_line = lineno;
 
 	view_border(view);
+
+	if (tog_io.wait_for_ui) {
+		if (s->blame_complete)
+			tog_io.wait_for_ui = 0;
+	}
 
 	return NULL;
 }
