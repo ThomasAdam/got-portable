cdfe5501a3d4265bab3c6f877f19f37a4b20b0da
Stefan Sperling <stsp@stsp.name>
2023-04-24 12:33:54 +0000

    when finding changed paths iterate tree entries in on-disk order for speed
    ok op, jamsek

--- lib/got_lib_object_parse.h
+++ lib/got_lib_object_parse.h
@@ -32,6 +32,8 @@ struct got_parsed_tree_entry {
 	mode_t mode; /* Mode parsed from tree buffer. */
 	uint8_t *id; /* Points to ID in parsed tree buffer. */
 };
+const struct got_error *got_object_parse_tree_entry(
+    struct got_parsed_tree_entry *, size_t *, char *, size_t);
 const struct got_error *got_object_parse_tree(struct got_parsed_tree_entry **,
     size_t *, size_t *, uint8_t *, size_t);
 const struct got_error *got_object_read_tree(struct got_parsed_tree_entry **,
