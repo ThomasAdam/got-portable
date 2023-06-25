f528b33ecc8d05209f34af6472acdbc70f51d285
Christian Weisgerber <naddy@mips.inka.de>
2023-06-25 15:01:50 +0000

    use _POSIX_HOST_NAME_MAX from <limits.h> for portability
    ok stsp

--- lib/repository_admin.c
+++ lib/repository_admin.c
@@ -618,7 +618,7 @@ got_repo_cleanup_prepare(struct got_repository *repo,
     struct got_lockfile **lk)
 {
 	const struct got_error *err;
-	char myname[HOST_NAME_MAX + 1];
+	char myname[_POSIX_HOST_NAME_MAX + 1];
 
 	if (gethostname(myname, sizeof(myname)) == -1)
 		return got_error_from_errno("gethostname");
