d7a689ea907503c998ff3b75af65066ce8ec680d
Christian Weisgerber <naddy@mips.inka.de>
2023-10-01 09:19:06 +0000

    honor fetch_all_branches configuration again
    Fix a regression in db82695e31945b3ce001a5fb69674c3638622368 where
    the fetch_all_branches configuration setting from got.conf was dropped
    and always defaulted to "no".
    
    Add corresponding regression test.
    
    ok stsp

--- lib/repository.c
+++ lib/repository.c
@@ -941,6 +941,8 @@ got_repo_remote_repo_dup(struct got_remote_repo **newp,
 
 	new->mirror_references = repo->mirror_references;
 
+	new->fetch_all_branches = repo->fetch_all_branches;
+
 	new->nfetch_branches = repo->nfetch_branches;
 	if (repo->fetch_branches) {
 		new->fetch_branches = calloc(repo->nfetch_branches,
