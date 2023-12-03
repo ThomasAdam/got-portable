b2424a42ae549628fe7e8ac9bf5b69af89f59d46
Mark Jamsek <mark@jamsek.dev>
2023-12-03 12:08:33 +0000

    plug got status worktree leak found by Kyle Ackerman; ok op@

--- got/got.c
+++ got/got.c
@@ -6407,7 +6407,7 @@ print:
 static const struct got_error *
 cmd_status(int argc, char *argv[])
 {
-	const struct got_error *error = NULL;
+	const struct got_error *close_err, *error = NULL;
 	struct got_repository *repo = NULL;
 	struct got_worktree *worktree = NULL;
 	struct got_status_arg st;
@@ -6511,7 +6511,12 @@ done:
 			error = pack_err;
 	}
 	if (repo) {
-		const struct got_error *close_err = got_repo_close(repo);
+		close_err = got_repo_close(repo);
+		if (error == NULL)
+			error = close_err;
+	}
+	if (worktree != NULL) {
+		close_err = got_worktree_close(worktree);
 		if (error == NULL)
 			error = close_err;
 	}
