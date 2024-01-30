f03e50d6fa7d457cde6feabb23a528f11bb45461
Omar Polo <op@omarpolo.com>
2024-01-30 10:26:42 +0000

    gotwebd: guard against missig folder and file parameter in BLOB and BLAME
    ok stsp

--- gotwebd/gotweb.c
+++ gotwebd/gotweb.c
@@ -205,6 +205,11 @@ gotweb_process_request(struct request *c)
 	}
 
 	if (qs->action == BLOBRAW || qs->action == BLOB) {
+		if (qs->folder == NULL || qs->file == NULL) {
+			error = got_error(GOT_ERR_BAD_QUERYSTRING);
+			goto err;
+		}
+
 		error = got_get_repo_commits(c, 1);
 		if (error)
 			goto err;
@@ -217,6 +222,10 @@ gotweb_process_request(struct request *c)
 
 	switch (qs->action) {
 	case BLAME:
+		if (qs->folder == NULL || qs->file == NULL) {
+			error = got_error(GOT_ERR_BAD_QUERYSTRING);
+			goto err;
+		}
 		error = got_get_repo_commits(c, 1);
 		if (error) {
 			log_warnx("%s: %s", __func__, error->msg);
