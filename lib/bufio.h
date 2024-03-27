83fccf2ddf25d8a98b076afeb22dfa6d7f6aaad2
Omar Polo <op@omarpolo.com>
2024-03-27 19:29:40 +0000

    add my name in the bufio sources

--- lib/bufio.h
+++ lib/bufio.h
@@ -1,4 +1,6 @@
 /*
+ * bufio.h was written by Omar Polo <op@omarpolo.com>
+ *
  * This is free and unencumbered software released into the public domain.
  *
  * Anyone is free to copy, modify, publish, use, compile, sell, or
