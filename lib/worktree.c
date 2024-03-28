a854b8c994a1381e4c6f2ba45bef9c22da0ec956
Stefan Sperling <stsp@stsp.name>
2024-03-28 13:49:18 +0000

    initialize and tidy up *branch_ref in got_worktree_histedit_continue()

--- lib/worktree.c
+++ lib/worktree.c
@@ -7948,6 +7948,7 @@ got_worktree_histedit_continue(struct got_object_id **commit_id,
 
 	*commit_id = NULL;
 	*tmp_branch = NULL;
+	*branch_ref = NULL;
 	*base_commit_id = NULL;
 	*fileindex = NULL;
 
@@ -8022,6 +8023,10 @@ done:
 		if (*tmp_branch) {
 			got_ref_close(*tmp_branch);
 			*tmp_branch = NULL;
+		}
+		if (*branch_ref) {
+			got_ref_close(*branch_ref);
+			*branch_ref = NULL;
 		}
 		if (*fileindex) {
 			got_fileindex_free(*fileindex);
