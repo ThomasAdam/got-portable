1e5b87a027a03fdf4e2c197e5e3ffe5ddd16fc11
Thomas Adam <thomas@xteddy.org>
2024-06-03 20:22:06 +0100

    portable: release 0.100

--- util/got-portable-ver.sh
+++ util/got-portable-ver.sh
@@ -7,7 +7,7 @@
 #		    If it's a developer build, it's left as-is.
 #
 # Intended to be called from configure.ac (via autogen.sh)
-GOT_RELEASE=no
+GOT_RELEASE=yes
 GOT_PORTABLE_VER=0.100
 
 [ -d ".git" -a "$GOT_RELEASE" = "no" ] || { echo "$GOT_PORTABLE_VER" ; exit ; }
