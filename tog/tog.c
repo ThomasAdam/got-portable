06806d1d9d0b5e8568dc875cecc87af4e2ad55df
Omar Polo <op@omarpolo.com>
2024-07-12 18:34:55 +0000

    backout 4f73a04b63 "Remove unnecessary sched_yield(2)"
    this made tog regress hang in an infinite loop, reported by naddy.
    Remove unnecessary sched_yield(2).
    
    Re-applies commit 6b8a2b8fcd99c4365b1aa9513c0f0149beac2491 which was
    reverted in commit 82954512f323c8a2d4f89d51be1e6b0f707b6c3a because
    at the time the change broke the 'search' feature.
    Quick testing suggests search is now working fine, so let's try again.

--- tog/tog.c
+++ tog/tog.c
@@ -42,6 +42,7 @@
 #include <pthread.h>
 #include <libgen.h>
 #include <regex.h>
+#include <sched.h>
 
 #include "got_version.h"
 #include "got_error.h"
@@ -1720,6 +1721,15 @@ view_input(struct tog_view **new, int *done, struct tog_view *view,
 	}
 
 	if (view->searching && !view->search_next_done) {
+		errcode = pthread_mutex_unlock(&tog_mutex);
+		if (errcode)
+			return got_error_set_errno(errcode,
+			    "pthread_mutex_unlock");
+		sched_yield();
+		errcode = pthread_mutex_lock(&tog_mutex);
+		if (errcode)
+			return got_error_set_errno(errcode,
+			    "pthread_mutex_lock");
 		view->search_next(view);
 		return NULL;
 	}
