9530fd041d6cad2b6bd30d1e0ae2ea57e557e2b0
Stefan Sperling <stsp@stsp.name>
2024-11-21 21:57:16 +0000

    do not call imsgbuf_clear() before got_privsep_send_error() in helpers
    Fixes cosmetic test failures, such as in cmdline/tree.sh.

--- libexec/got-read-pack/got-read-pack.c
+++ libexec/got-read-pack/got-read-pack.c
@@ -2146,7 +2146,6 @@ main(int argc, char *argv[])
 	if (pack)
 		got_pack_close(pack);
 	got_object_cache_close(&objcache);
-	imsgbuf_clear(&ibuf);
 	if (basefile && fclose(basefile) == EOF && err == NULL)
 		err = got_error_from_errno("fclose");
 	if (accumfile && fclose(accumfile) == EOF && err == NULL)
@@ -2159,6 +2158,7 @@ main(int argc, char *argv[])
 			got_privsep_send_error(&ibuf, err);
 		}
 	}
+	imsgbuf_clear(&ibuf);
 	if (close(GOT_IMSG_FD_CHILD) == -1 && err == NULL)
 		err = got_error_from_errno("close");
 	return err ? 1 : 0;
