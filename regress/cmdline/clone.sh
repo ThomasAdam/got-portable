33fc334f899c8aea40a606e250bd06010d094c1b
Stefan Sperling <stsp@stsp.name>
2024-11-22 14:27:39 +0000

    grant regress http-server script more time to start up
    Otherwise I see spurious test failures on my arm64 builder.

--- regress/cmdline/clone.sh
+++ regress/cmdline/clone.sh
@@ -867,11 +867,11 @@ test_clone_basic_http() {
 	local testurl=http://127.0.0.1:${GOT_TEST_HTTP_PORT}
 	local commit_id=`git_show_head $testroot/repo`
 
-	timeout 5 ./http-server -p $GOT_TEST_HTTP_PORT $testroot \
+	timeout 10 ./http-server -p $GOT_TEST_HTTP_PORT $testroot \
 	    > $testroot/http-server.log &
 	trap "kill %1" HUP INT QUIT PIPE TERM
 
-	sleep 1 # server starts up
+	sleep 2 # server starts up
 
 	# Test our custom HTTP server with git clone. Should succeed.
 	git clone -q $testurl/repo $testroot/repo-clone-with-git
