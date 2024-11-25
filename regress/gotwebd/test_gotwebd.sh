9210d298790149ecda176baadae4b1325b1113b5
Mark Jamsek <mark@jamsek.dev>
2024-11-25 10:18:13 +0000

    gotwebd: basic blame and tree action regress; ok stsp@

--- regress/gotwebd/test_gotwebd.sh
+++ regress/gotwebd/test_gotwebd.sh
@@ -73,6 +73,63 @@ test_gotwebd_action_commit()
 	test_done "$testroot" "$repo" "$ret"
 }
 
+test_gotwebd_action_blame()
+{
+	local testroot=$(test_init gotwebd_action_blame 1)
+	local repo="${GOTWEBD_TEST_CHROOT}/got/public/repo.git"
+	local id=$(git_show_head $repo)
+	local author_time=$(git_show_author_time $repo)
+	local qs="action=blame&commit=${id}&file=alpha&folder=&path=repo.git"
+
+	COMMIT_ID=$id \
+	COMMITTER="flan_hack" \
+	COMMIT_ID8=$(printf '%.8s' $id) \
+	COMMIT_YMD=$(date -u -r $author_time +"%F") \
+	COMMIT_YMDHMS=$(date -u -r $author_time +"%FT%TZ") \
+	COMMIT_DATE=$(date -u -r $author_time +"%a %b %e %X %Y") \
+	interpolate action_blame.html > $testroot/content.expected
+
+	$GOTWEBD_TEST_FCGI -q "$qs" > $testroot/content
+
+	cmp -s $testroot/content.expected $testroot/content
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		diff -u $testroot/content.expected $testroot/content
+		test_done "$testroot" "$repo" "$ret"
+		return 1
+	fi
+
+	test_done "$testroot" "$repo" "$ret"
+}
+
+test_gotwebd_action_tree()
+{
+	local testroot=$(test_init gotwebd_action_tree 1)
+	local repo="${GOTWEBD_TEST_CHROOT}/got/public/repo.git"
+	local author_time=$(git_show_author_time $repo)
+	local qs="action=tree&path=repo.git"
+
+	COMMIT_ID=$(git_show_head $repo) \
+	COMMIT_YMDHMS=$(date -u -r $author_time +"%FT%TZ") \
+	COMMIT_DATE=$(date -u -r $author_time +"%a %b %e %X %Y") \
+	TREE_ID=$(got cat -r $repo main | head -1 | cut -d ' ' -f2) \
+	interpolate action_tree.html > $testroot/content.expected
+
+	$GOTWEBD_TEST_FCGI -q "$qs" > $testroot/content
+
+	cmp -s $testroot/content.expected $testroot/content
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		diff -u $testroot/content.expected $testroot/content
+		test_done "$testroot" "$repo" "$ret"
+		return 1
+	fi
+
+	test_done "$testroot" "$repo" "$ret"
+}
+
 test_parseargs "$@"
 run_test test_gotwebd_action_index
 run_test test_gotwebd_action_commit
+run_test test_gotwebd_action_blame
+run_test test_gotwebd_action_tree
