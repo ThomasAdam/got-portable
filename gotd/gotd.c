ThomasAdam/got-portable

commit dda4473541d84973dd43a16f2f79ff0ce5f93dfe

make gotd run 'gotsys check' on gotsys.conf commits before accepting them
For now this check is quite strict. Any commit which does not make a change
to gotsys.conf will be rejected. If users want to keep other files in the
gotsys.git repo we'll need to relax these checks a little.

gotd/gotd.c
@@ -24,6 +24,7 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 
+#include <ctype.h>
 #include <fcntl.h>
 #include <err.h>
 #include <errno.h>
@@ -105,7 +106,9 @@ struct gotd_client {
 	struct gotd_child_proc		*repo;
 	struct gotd_child_proc		*auth;
 	struct gotd_child_proc		*session;
+	struct gotd_child_proc		*gotsys;
 	int				 required_auth;
+	int				 gotsys_error_sent;
 };
 STAILQ_HEAD(gotd_clients, gotd_client);
 
@@ -243,6 +246,8 @@ find_client_by_proc_fd(int fd)
 				return c;
 			if (c->session && c->session->iev.ibuf.fd == fd)
 				return c;
+			if (c->gotsys && c->gotsys->pipe[0] == fd)
+				return c;
 		}
 	}
 
@@ -296,6 +301,8 @@ proc_done(struct gotd_child_proc *proc)
 	TAILQ_REMOVE(&procs, proc, entry);
 
 	client = find_client_by_proc_fd(proc->iev.ibuf.fd);
+	if (client == NULL)
+		client = find_client_by_proc_fd(proc->pipe[0]);
 	if (client != NULL) {
 		if (proc == client->repo)
 			client->repo = NULL;
@@ -303,7 +310,8 @@ proc_done(struct gotd_child_proc *proc)
 			client->auth = NULL;
 		if (proc == client->session)
 			client->session = NULL;
-		disconnect(client);
+		if (proc == client->gotsys)
+			client->gotsys = NULL;
 	}
 
 	if (proc == gotd.notify_proc)
@@ -351,6 +359,16 @@ kill_session_proc(struct gotd_client *client)
 }
 
 static void
+kill_gotsys_proc(struct gotd_client *client)
+{
+	if (client->gotsys == NULL)
+		return;
+
+	kill_proc(client->gotsys, 0);
+	client->gotsys = NULL;
+}
+
+static void
 disconnect(struct gotd_client *client)
 {
 	struct gotd_imsg_disconnect idisconnect;
@@ -362,6 +380,7 @@ disconnect(struct gotd_client *client)
 	kill_auth_proc(client);
 	kill_session_proc(client);
 	kill_repo_proc(client);
+	kill_gotsys_proc(client);
 
 	idisconnect.client_id = client->id;
 	if (gotd_imsg_compose_event(&listen_proc->iev,
@@ -820,9 +839,44 @@ find_proc_by_pid(pid_t pid)
 	return proc;
 }
 
+static const struct got_error *
+gotsys_exit(struct gotd_child_proc *proc, int status)
+{
+	struct gotd_client *client;
+
+	log_debug("gotsys check (PID %d) %s", proc->pid,
+	    WEXITSTATUS(status) == 0 ? "succeeded" : "failed"); 
+
+	client = find_client_by_proc_fd(proc->pipe[0]);
+	if (client == NULL)
+		return NULL;
+
+	if (client->session == NULL)
+		return NULL;
+
+	if (WEXITSTATUS(status) == 0) {
+		if (gotd_imsg_compose_event(&client->session->iev,
+		    GOTD_IMSG_PACKFILE_VERIFIED, GOTD_PROC_GOTD,
+		    -1, NULL, 0) == -1)
+			return got_error_from_errno("imsg compose "
+			    "PACKFILE_VERIFIED");
+	} else if (!client->gotsys_error_sent && client->session != NULL) {
+		const struct got_error *err;
+
+		err = got_error_msg(GOT_ERR_PARSE_CONFIG,
+		    "gotsys check failure");
+		if (gotd_imsg_send_error_event(&client->session->iev,
+		    GOTD_PROC_GOTD, client->id, err) == -1)
+			log_warn("imsg send error");
+	}
+
+	return NULL;
+}
+
 void
 gotd_sighdlr(int sig, short event, void *arg)
 {
+	const struct got_error *err;
 	struct gotd_child_proc *proc;
 	pid_t pid;
 	int status;
@@ -867,6 +921,12 @@ gotd_sighdlr(int sig, short event, void *arg)
 			if (WIFSIGNALED(status)) {
 				log_warnx("child PID %d terminated with"
 				    " signal %d", pid, WTERMSIG(status));
+			}
+
+			if (proc->type == GOTD_PROC_GOTSYS) {
+				err = gotsys_exit(proc, status);
+				if (err)
+					log_warn("%s", err->msg);
 			}
 
 			proc_done(proc);
@@ -996,6 +1056,19 @@ verify_imsg_src(struct gotd_client *client, struct gotd_child_proc *proc,
 		if (err)
 			log_warnx("uid %d: %s", client->euid, err->msg);
 		else
+			ret = 1;
+		break;
+	case GOTD_IMSG_RUN_GOTSYS_CHECK:
+		if (proc->type != GOTD_PROC_SESSION_WRITE) {
+			err = got_error_fmt(GOT_ERR_BAD_PACKET,
+			    "unexpected \"ready\" signal from PID %d",
+			    proc->pid);
+			break;
+		}
+		err = ensure_proc_is_writing(client, proc);
+		if (err) {
+			log_warnx("uid %d: %s", client->euid, err->msg);
+		}
 			ret = 1;
 		break;
 	default:
@@ -1370,6 +1443,130 @@ connect_session(struct gotd_client *client)
 }
 
 static void
+gotd_read_gotsys_check_stderr(int fd, short event, void *arg)
+{
+	const struct got_error *err = NULL;
+	struct gotd_child_proc *proc = arg;
+	struct gotd_client *client;
+	ssize_t n, i;
+	char buf[1024];
+
+	memset(buf, 0, sizeof(buf));
+
+	log_debug("%s", __func__);
+
+	if (event & EV_READ) {
+		n = read(fd, buf, sizeof(buf) - 1 /* keep a trailing NUL */);
+		if (n == 0) /* stderr pipe closed */
+			goto done;
+
+		/* Deliver the 'gotsys check' error to the client session. */
+		for (i = 0; i < n; i++) {
+			if (!isprint(buf[i])) {
+				buf[i] = '\0';
+				break;
+			}
+		}
+		err = got_error_msg(GOT_ERR_PARSE_CONFIG, buf);
+		log_warnx("gotsys check: %s", err->msg);
+
+		client = find_client_by_proc_fd(fd);
+		if (client == NULL) {
+			/* Can happen during process teardown. */
+			warnx("cannot find client for fd %d", fd);
+			goto done;
+		}
+
+		if (client->session != NULL &&
+		    gotd_imsg_send_error_event(&client->session->iev,
+		    GOTD_PROC_GOTD, client->id, err) == -1)
+			log_warn("imsg send error");
+		else
+			client->gotsys_error_sent = 1;
+	}
+done:
+	event_del(&proc->iev.ev);
+}
+
+static const struct got_error *
+run_gotsys_check(struct gotd_client *client, struct gotd_repo *repo,
+    int content_fd)
+{
+	struct gotd_child_proc *proc;
+	const char	*argv[4];
+	int		 argc = 0;
+	pid_t		 pid;
+
+	proc = calloc(1, sizeof(*proc));
+	if (proc == NULL)
+		return got_error_from_errno("calloc");
+
+	proc->type = GOTD_PROC_GOTSYS;
+	if (strlcpy(proc->repo_name, repo->name,
+	    sizeof(proc->repo_name)) >= sizeof(proc->repo_name))
+		fatalx("repository name too long: %s", repo->name);
+	if (strlcpy(proc->repo_path, repo->path, sizeof(proc->repo_path)) >=
+	    sizeof(proc->repo_path))
+		fatalx("repository path too long: %s", repo->path);
+
+	if (socketpair(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC|SOCK_NONBLOCK,
+	    PF_UNSPEC, proc->pipe) == -1) {
+		free(proc);
+		return got_error_from_errno("socketpair");
+	}
+
+	proc->iev.handler = gotd_read_gotsys_check_stderr;
+	proc->iev.events = EV_READ;
+	proc->iev.handler_arg = NULL;
+	event_set(&proc->iev.ev, proc->pipe[0], EV_READ,
+	    gotd_read_gotsys_check_stderr, proc);
+	event_add(&proc->iev.ev, NULL);
+
+	TAILQ_INSERT_HEAD(&procs, proc, entry);
+
+	evtimer_set(&proc->tmo, kill_proc_timeout, proc);
+
+	switch (pid = fork()) {
+	case -1:
+		fatal("cannot fork");
+	case 0:
+		break;
+	default:
+		proc->pid = pid;
+		close(proc->pipe[1]);
+		proc->pipe[1] = -1;
+		client->gotsys = proc;
+		log_debug("running gotsys check (PID %d)", pid);
+		return NULL;
+	}
+
+	if (content_fd != STDIN_FILENO) {
+		    if (dup2(content_fd, STDIN_FILENO) == -1)
+			fatal("cannot redirect stdin");
+	} else if (fcntl(content_fd, F_SETFD, 0) == -1)
+		fatal("cannot fcntl stdin");
+
+	if (proc->pipe[1] != STDERR_FILENO) {
+		if (dup2(proc->pipe[1], STDERR_FILENO) == -1)
+			fatal("cannot redirect stderr");
+	} else if (fcntl(proc->pipe[1], F_SETFD, 0) == -1)
+		fatal("cannot fcntl stderr");
+
+	closefrom(STDERR_FILENO + 1);
+
+	argv[argc++] = GOTD_PATH_PROG_GOTSYS;
+	argv[argc++] = "check";
+	argv[argc++] = "-f";
+	argv[argc++] = "-";
+	argv[argc++] = NULL;
+
+	execvp(argv[0], (char * const *)argv);
+	fatal("execvp");
+	/* NOTREACHED */
+	return NULL; 
+}
+
+static void
 run_gotsys_apply(struct gotd_repo *repo)
 {
 	struct gotd_child_proc *proc;
@@ -1402,6 +1599,7 @@ run_gotsys_apply(struct gotd_repo *repo)
 		break;
 	default:
 		proc->pid = pid;
+		log_debug("running gotsys apply (PID %d)", pid);
 		return;
 	}
 
@@ -1461,7 +1659,8 @@ gotd_dispatch_client_session(int fd, short event, void *arg)
 		const struct got_error *err = NULL;
 		uint32_t client_id = 0;
 		int do_disconnect = 0, do_start_repo_child = 0;
-		int refs_updated = 0;
+		int do_gotsys_check = 0, refs_updated = 0;
+		int fd = -1;
 
 		if ((n = imsg_get(ibuf, &imsg)) == -1)
 			fatal("%s: imsg_get error", __func__);
@@ -1479,6 +1678,18 @@ gotd_dispatch_client_session(int fd, short event, void *arg)
 				break;
 			}
 			do_start_repo_child = 1;
+			break;
+		case GOTD_IMSG_RUN_GOTSYS_CHECK:
+			if (client->state != GOTD_CLIENT_STATE_ACCESS_GRANTED) {
+				err = got_error(GOT_ERR_PRIVSEP_MSG);
+				break;
+			}
+			fd = imsg_get_fd(&imsg);
+			if (fd == -1) {
+				err = got_error(GOT_ERR_PRIVSEP_NO_FD);
+				break;
+			}
+			do_gotsys_check = 1;
 			break;
 		case GOTD_IMSG_REFS_UPDATED:
 			if (client->state != GOTD_CLIENT_STATE_ACCESS_GRANTED) {
@@ -1526,6 +1737,20 @@ gotd_dispatch_client_session(int fd, short event, void *arg)
 			if (err) {
 				log_warnx("uid %d: %s", client->euid, err->msg);
 				do_disconnect = 1;
+			}
+		}
+
+		if (do_gotsys_check) {
+			struct gotd_repo *repo;
+
+			repo = gotd_find_repo_by_name("gotsys", &gotd.repos);
+			if (repo == NULL)
+				repo = gotd_find_repo_by_name("gotsys.git",
+				    &gotd.repos);
+			if (repo) {
+				err = run_gotsys_check(client, repo, fd);
+				if (err)
+					do_disconnect = 1;
 			}
 		}
 
@@ -2132,8 +2357,8 @@ main(int argc, char **argv)
 	char *default_sender = NULL;
 	char hostname[_POSIX_HOST_NAME_MAX + 1];
 	FILE *fp;
-	FILE *diff_f1 = NULL, *diff_f2 = NULL;
-	int diff_fd1 = -1, diff_fd2 = -1;
+	FILE *diff_f1 = NULL, *diff_f2 = NULL, *tmp_f1 = NULL, *tmp_f2 = NULL;
+	int diff_fd1 = -1, diff_fd2 = -1, tmp_fd = -1;
 	const char *errstr;
 
 	TAILQ_INIT(&procs);
@@ -2394,6 +2619,9 @@ main(int argc, char **argv)
 		/* NOTREACHED */
 		break;
 	case GOTD_PROC_SESSION_WRITE:
+		tmp_fd = got_opentempfd();
+		if (tmp_fd == -1)
+			fatal("got_opentempfd");
 #ifndef PROFILE
 		/*
 		 * The "recvfd" promise is only needed during setup and
@@ -2407,7 +2635,7 @@ main(int argc, char **argv)
 		repo = gotd_find_repo_by_path(repo_path, &gotd);
 		if (repo == NULL)
 			fatalx("no repository for path %s", repo_path);
-		session_write_main(title, repo_path, pack_fds, temp_fds,
+		session_write_main(title, repo_path, pack_fds, temp_fds, tmp_fd,
 		    &gotd.request_timeout, repo);
 		/* NOTREACHED */
 		break;
@@ -2436,6 +2664,12 @@ main(int argc, char **argv)
 		diff_fd2 = got_opentempfd();
 		if (diff_fd2 == -1)
 			fatal("got_opentempfd");
+		tmp_f1 = got_opentemp();
+		if (tmp_f1 == NULL)
+			fatal("got_opentemp");
+		tmp_f2 = got_opentemp();
+		if (tmp_f2 == NULL)
+			fatal("got_opentemp");
 #ifndef PROFILE
 		if (pledge("stdio rpath recvfd unveil", NULL) == -1)
 			err(1, "pledge");
@@ -2445,7 +2679,7 @@ main(int argc, char **argv)
 		if (repo == NULL)
 			fatalx("no repository for path %s", repo_path);
 		repo_write_main(title, repo_path, pack_fds, temp_fds,
-		    diff_f1, diff_f2, diff_fd1, diff_fd2,
+		    tmp_f1, tmp_f2, diff_f1, diff_f2, diff_fd1, diff_fd2,
 		    &repo->protected_tag_namespaces,
 		    &repo->protected_branch_namespaces,
 		    &repo->protected_branches);

gotd/gotd.h
@@ -216,6 +216,7 @@ enum gotd_imsg_type {
 	/* Sending or receiving a pack file. */
 	GOTD_IMSG_SEND_PACKFILE, /* The server is sending a pack file. */
 	GOTD_IMSG_RECV_PACKFILE, /* The server is receiving a pack file. */
+	GOTD_IMSG_PACKFILE_RECEIVED,
 	GOTD_IMSG_PACKIDX_FILE,  /* Temporary file handle for new pack index. */
 	GOTD_IMSG_PACKFILE_PIPE, /* Pipe to send/receive a pack file stream. */
 	GOTD_IMSG_PACKFILE_PROGRESS, /* Progress reporting. */
@@ -223,6 +224,12 @@ enum gotd_imsg_type {
 	GOTD_IMSG_PACKFILE_STATUS, /* Received pack success/failure status. */
 	GOTD_IMSG_PACKFILE_INSTALL, /* Received pack file can be installed. */
 	GOTD_IMSG_PACKFILE_DONE, /* Pack file has been sent/received. */
+
+	/* Pack file content verification. */
+	GOTD_IMSG_PACKFILE_GET_CONTENT,
+	GOTD_IMSG_PACKFILE_CONTENT_WRITTEN,
+	GOTD_IMSG_RUN_GOTSYS_CHECK,
+	GOTD_IMSG_PACKFILE_VERIFIED,
 
 	/* Reference updates. */
 	GOTD_IMSG_REF_UPDATES_START, /* Ref updates starting. */
@@ -378,6 +385,30 @@ struct gotd_imsg_ref_update {
 	/* Followed by name_len data bytes. */
 } __attribute__((__packed__));
 
+/* Structure for GOTD_IMSG_PACKFILE_GET_CONTENT. */
+struct gotd_imsg_packfile_get_content {
+	size_t refname_len;
+	size_t path_len;
+
+	/* Followed by refname_len + path_len data bytes. */
+
+	/* Content file descriptor is passed via imsg. */
+};
+
+/* Structure for GOTD_IMSG_PACKFILE_CONTENT_WRITTEN. */
+struct gotd_imsg_packfile_content_written {
+	/* If zero, the requested reference is not being updated. */
+	int ref_found;
+
+	/*
+	 * If wrote_content is zero, nothing was found at the requested path.
+	 *
+	 * Else, content as it appears in the pack file was written to the
+	 * file descriptor. If this content is empty, the file will be empty.
+	 */
+	int wrote_content;
+};
+
 /* Structure for GOTD_IMSG_REF_UPDATES_START data. */
 struct gotd_imsg_ref_updates_start {
 	int nref_updates;
@@ -419,6 +450,11 @@ struct gotd_imsg_recv_packfile {
 	int report_status;
 
 	/* pack destination temp file is sent as a file descriptor */
+};
+
+/* Structure for GOTD_IMSG_PACKFILE_RECEIVED data. */
+struct gotd_imsg_packfile_received {
+	int pack_empty;
 };
 
 /*

gotd/repo_write.c
@@ -74,6 +74,8 @@ static struct repo_write {
 	struct got_repository *repo;
 	int *pack_fds;
 	int *temp_fds;
+	FILE *base_file;
+	FILE *accum_file;
 	int session_fd;
 	struct gotd_imsgev session_iev;
 	struct got_pathlist_head *protected_tag_namespaces;
@@ -86,6 +88,7 @@ static struct repo_write {
 		int fd2;
 	} diff;
 	int refs_listed;
+	int have_packfile;
 } repo_write;
 
 struct gotd_ref_update {
@@ -103,6 +106,7 @@ static struct repo_write_client {
 	int				 fd;
 	int				 pack_pipe;
 	struct got_pack			 pack;
+	struct got_packidx		 *packidx;
 	uint8_t				 pack_sha1[SHA1_DIGEST_LENGTH];
 	int				 packidx_fd;
 	struct gotd_ref_updates		 ref_updates;
@@ -1364,10 +1368,9 @@ done:
 static const struct got_error *
 verify_packfile(void)
 {
-	const struct got_error *err = NULL, *close_err;
+	const struct got_error *err = NULL;
 	struct repo_write_client *client = &repo_write_client;
 	struct gotd_ref_update *ref_update;
-	struct got_packidx *packidx = NULL;
 	struct stat sb;
 	char *id_str = NULL;
 	struct got_object *obj = NULL;
@@ -1391,14 +1394,18 @@ verify_packfile(void)
 	if (fstat(client->packidx_fd, &sb) == -1)
 		return got_error_from_errno("pack index fstat");
 
-	packidx = calloc(1, sizeof(*packidx));
-	if (packidx == NULL)
-		return got_error_from_errno("calloc");
-	packidx->fd = client->packidx_fd;
-	client->packidx_fd = -1;
-	packidx->len = sb.st_size;
+	if (client->packidx != NULL)
+		return got_error(GOT_ERR_PRIVSEP_MSG);
 
-	err = got_packidx_init_hdr(packidx, 1, client->pack.filesize);
+	client->packidx = calloc(1, sizeof(*client->packidx));
+	if (client->packidx == NULL)
+		return got_error_from_errno("calloc");
+
+	client->packidx->fd = client->packidx_fd;
+	client->packidx_fd = -1;
+	client->packidx->len = sb.st_size;
+
+	err = got_packidx_init_hdr(client->packidx, 1, client->pack.filesize);
 	if (err)
 		return err;
 
@@ -1409,7 +1416,7 @@ verify_packfile(void)
 		RB_FOREACH(pe, got_pathlist_head,
 		    repo_write.protected_tag_namespaces) {
 			err = protect_tag_namespace(pe->path, &client->pack,
-			    packidx, ref_update);
+			    client->packidx, ref_update);
 			if (err)
 				goto done;
 		}
@@ -1427,7 +1434,7 @@ verify_packfile(void)
 			got_object_close(obj);
 			obj = NULL;
 		} else {
-			int idx = got_packidx_get_object_idx(packidx,
+			int idx = got_packidx_get_object_idx(client->packidx,
 			    &ref_update->new_id);
 			if (idx == -1) {
 				got_object_id_hex(&ref_update->new_id,
@@ -1442,23 +1449,20 @@ verify_packfile(void)
 		RB_FOREACH(pe, got_pathlist_head,
 		    repo_write.protected_branch_namespaces) {
 			err = protect_branch_namespace(pe->path,
-			    &client->pack, packidx, ref_update);
+			    &client->pack, client->packidx, ref_update);
 			if (err)
 				goto done;
 		}
 		RB_FOREACH(pe, got_pathlist_head,
 		    repo_write.protected_branches) {
 			err = protect_branch(pe->path, &client->pack,
-			    packidx, ref_update);
+			    client->packidx, ref_update);
 			if (err)
 				goto done;
 		}
 	}
 
 done:
-	close_err = got_packidx_close(packidx);
-	if (close_err && err == NULL)
-		err = close_err;
 	free(id_str);
 	if (obj)
 		got_object_close(obj);
@@ -2208,6 +2212,345 @@ done:
 	return err;
 }
 
+static const struct got_error *
+send_packfile_received(struct gotd_imsgev *iev, int have_packfile)
+{
+	struct gotd_imsg_packfile_received recvd;
+	int ret;
+
+	if (have_packfile)
+		recvd.pack_empty = 0;
+	else
+		recvd.pack_empty = 1;
+
+	ret = gotd_imsg_compose_event(iev, GOTD_IMSG_PACKFILE_RECEIVED,
+	    GOTD_PROC_REPO_WRITE, -1, &recvd, sizeof(recvd));
+	if (ret == -1)
+		return got_error_from_errno("imsg_compose PACKFILE_RECEIVED");
+
+	return NULL;
+}
+
+static const struct got_error *
+open_tree(struct got_tree_object **tree, struct got_pack *pack,
+    struct got_packidx *packidx, int idx, struct got_object_id *tree_id)
+{
+	const struct got_error *err;
+	struct got_parsed_tree_entry *entries = NULL;
+	size_t nentries = 0, nentries_alloc = 0, i;
+	struct got_object *obj = NULL;
+	uint8_t *buf = NULL;
+	size_t len;
+
+	*tree = NULL;
+
+	err = got_packfile_open_object(&obj, pack, packidx, idx, tree_id);
+	if (err)
+		return err;
+
+	if (obj->type != GOT_OBJ_TYPE_TREE) {
+		err = got_error(GOT_ERR_OBJ_TYPE);
+		goto done;
+	}
+
+	err = got_packfile_extract_object_to_mem(&buf, &len, obj, pack);
+	if (err)
+		goto done;
+	
+	err = got_object_parse_tree(&entries, &nentries, &nentries_alloc,
+	    buf, len, GOT_HASH_SHA1);
+	if (err)
+		goto done;
+
+	*tree = malloc(sizeof(**tree));
+	if (*tree == NULL) {
+		err = got_error_from_errno("malloc");
+		goto done;
+	}
+	(*tree)->entries = calloc(nentries, sizeof(struct got_tree_entry));
+	if ((*tree)->entries == NULL) {
+		err = got_error_from_errno("malloc");
+		goto done;
+	}
+	(*tree)->nentries = nentries;
+	(*tree)->refcnt = 0;
+
+	for (i = 0; i < nentries; i++) {
+		struct got_parsed_tree_entry *pe = &entries[i];
+		struct got_tree_entry *te = &(*tree)->entries[i];
+
+		if (strlcpy(te->name, pe->name,
+		    sizeof(te->name)) >= sizeof(te->name)) {
+			err = got_error(GOT_ERR_NO_SPACE);
+			goto done;
+		}
+		memcpy(te->id.hash, pe->id, pe->digest_len);
+		te->id.algo = GOT_HASH_SHA1;
+		te->mode = pe->mode;
+		te->idx = i;
+	}
+done:
+	if (err && *tree) {
+		got_object_tree_close(*tree);
+		*tree = NULL;
+	}
+	got_object_close(obj);
+	free(buf);
+	return err;
+}
+
+static struct got_tree_entry *
+find_entry_by_name(struct got_tree_object *tree, const char *name, size_t len)
+{
+	int i;
+
+	/* Note that tree entries are sorted in strncmp() order. */
+	for (i = 0; i < tree->nentries; i++) {
+		struct got_tree_entry *te = &tree->entries[i];
+		int cmp = strncmp(te->name, name, len);
+		if (cmp < 0)
+			continue;
+		if (cmp > 0)
+			break;
+		if (te->name[len] == '\0')
+			return te;
+	}
+	return NULL;
+}
+
+static const struct got_error *
+find_id_by_path(struct got_object_id **id,
+    struct got_pack *pack, struct got_packidx *packidx,
+    struct got_tree_object *tree, const char *path)
+{
+	const struct got_error *err = NULL;
+	struct got_tree_object *subtree = NULL;
+	struct got_tree_entry *te = NULL;
+	const char *seg, *s;
+	size_t seglen;
+
+	*id = NULL;
+
+	s = path;
+	while (s[0] == '/')
+		s++;
+	seg = s;
+	seglen = 0;
+	subtree = tree;
+	while (*s) {
+		struct got_tree_object *next_tree;
+
+		if (*s != '/') {
+			s++;
+			seglen++;
+			if (*s)
+				continue;
+		}
+
+		te = find_entry_by_name(subtree, seg, seglen);
+		if (te == NULL)
+			break;
+
+		if (*s == '\0')
+			break;
+
+		seg = s + 1;
+		seglen = 0;
+		s++;
+		if (*s) {
+			int idx;
+
+			idx = got_packidx_get_object_idx(packidx, &te->id);
+			if (idx == -1)  {
+				te = NULL;
+				break;
+			}
+			err = open_tree(&next_tree, pack, packidx, idx,
+			    &te->id);
+			te = NULL;
+			if (err)
+				goto done;
+			if (subtree != tree)
+				got_object_tree_close(subtree);
+			subtree = next_tree;
+		}
+	}
+
+	if (te) {
+		*id = got_object_id_dup(&te->id);
+		if (*id == NULL)
+			return got_error_from_errno("got_object_id_dup");
+	} else
+		err = got_error_path(path, GOT_ERR_NO_TREE_ENTRY);
+done:
+	if (subtree && subtree != tree)
+		got_object_tree_close(subtree);
+	return err;
+}
+
+static const struct got_error *
+get_content_from_packfile(struct gotd_imsgev *iev, struct imsg *imsg)
+{
+	const struct got_error *err = NULL;
+	struct repo_write_client *client = &repo_write_client;
+	struct gotd_imsg_packfile_get_content content_req;
+	struct gotd_imsg_packfile_content_written written_resp;
+	size_t datalen;
+	char *refname = NULL;
+	char *path = NULL;
+	int fd = -1, idx;
+	struct gotd_ref_update *ref_update;
+	struct got_object_id *tree_id = NULL, *content_id = NULL;
+	struct got_object *obj = NULL;
+	uint8_t *buf = NULL;
+	size_t len;
+	struct got_commit_object *commit = NULL;
+	struct got_tree_object *tree = NULL;
+	FILE *outfile = NULL;
+
+	memset(&written_resp, 0, sizeof(written_resp));
+
+	datalen = imsg->hdr.len - IMSG_HEADER_SIZE;
+	if (datalen < sizeof(content_req))
+		return got_error(GOT_ERR_PRIVSEP_LEN);
+	memcpy(&content_req, imsg->data, sizeof(content_req)); 
+
+	if (content_req.path_len == 0 ||
+	    content_req.refname_len == 0 ||
+	    datalen != sizeof(content_req) + content_req.refname_len +
+	    content_req.path_len)
+		return got_error(GOT_ERR_PRIVSEP_LEN);
+	
+	refname = strndup(imsg->data + sizeof(content_req),
+	    content_req.refname_len);
+	if (strlen(refname) != content_req.refname_len) {
+		err = got_error(GOT_ERR_PRIVSEP_LEN);
+		goto done;
+	}
+
+	path = strndup(imsg->data + sizeof(content_req) +
+	    content_req.refname_len, content_req.path_len);
+	if (strlen(path) != content_req.path_len) {
+		err = got_error(GOT_ERR_PRIVSEP_LEN);
+		goto done;
+	}
+
+	if (got_path_is_root_dir(path)) {
+		err = got_error(GOT_ERR_PRIVSEP_MSG);
+		goto done;
+	}
+
+	fd = imsg_get_fd(imsg);
+	if (fd == -1) {
+		err = got_error(GOT_ERR_PRIVSEP_NO_FD);
+		goto done;
+	}
+
+	outfile = fdopen(fd, "w");
+	if (outfile == NULL) {
+		err = got_error_from_errno("fdopen");
+		goto done;
+	}
+	fd = -1;
+
+	STAILQ_FOREACH(ref_update, &client->ref_updates, entry) {
+		if (strcmp(got_ref_get_name(ref_update->ref), refname) == 0)
+			break;
+	}
+	if (ref_update == NULL)
+		goto send_response;
+
+	written_resp.ref_found = 1;	
+
+	idx = got_packidx_get_object_idx(client->packidx, &ref_update->new_id);
+	if (idx == -1) 
+		goto send_response;
+
+	err = got_packfile_open_object(&obj, &client->pack, client->packidx,
+	   idx, &ref_update->new_id);
+	if (err)
+		goto done;
+
+	if (obj->type != GOT_OBJ_TYPE_COMMIT)
+		goto send_response;
+
+	err = got_packfile_extract_object_to_mem(&buf, &len, obj,
+	    &client->pack);
+	if (err)
+		goto done;
+	got_object_close(obj);
+	obj = NULL;
+	
+	err = got_object_parse_commit(&commit, buf, len, GOT_HASH_SHA1);
+	if (err)
+		goto done;
+
+	tree_id = got_object_id_dup(commit->tree_id);
+	if (tree_id == NULL)
+		err = got_error_from_errno("got_object_id_dup");
+
+	got_object_commit_close(commit);
+	commit = NULL;
+	free(buf);
+	buf = NULL;
+	len = 0;
+
+	idx = got_packidx_get_object_idx(client->packidx, tree_id);
+	if (idx == -1) 
+		goto send_response;
+
+	err = open_tree(&tree, &client->pack, client->packidx, idx, tree_id);
+	if (err)
+		goto done;
+
+	err = find_id_by_path(&content_id, &client->pack, client->packidx,
+	    tree, path);
+	if (err)
+		goto done;
+
+	idx = got_packidx_get_object_idx(client->packidx, content_id);
+	if (idx == -1) 
+		goto send_response;
+
+	err = got_packfile_open_object(&obj, &client->pack, client->packidx,
+	   idx, content_id);
+	if (err)
+		goto done;
+
+	if (obj->type != GOT_OBJ_TYPE_BLOB)
+		goto send_response;
+
+	err = got_packfile_extract_object(&client->pack, obj, outfile,
+	    repo_write.base_file, repo_write.accum_file);
+	if (err)
+		goto done;
+	
+	written_resp.wrote_content = 1;	
+send_response:
+	if (imsg_compose(&iev->ibuf, GOTD_IMSG_PACKFILE_CONTENT_WRITTEN,
+	    GOTD_PROC_REPO_WRITE, repo_write.pid, -1, &written_resp,
+	    sizeof(written_resp)) == -1) {
+		err = got_error_from_errno("imsg_compose "
+		    "PACKFILE_CONTENT_WRITTEn");
+		goto done;
+	}
+	err = gotd_imsg_flush(&iev->ibuf);
+done:
+	if (fd != -1 && close(fd) == -1 && err == NULL)
+		err = got_error_from_errno("close");
+	if (outfile && fclose(outfile) == EOF && err == NULL)
+		err = got_error_from_errno("fclose");
+	free(refname);
+	free(path);
+	free(tree_id);
+	free(buf);
+	if (obj)
+		got_object_close(obj);
+	if (commit)
+		got_object_commit_close(commit);
+	return err;
+}
+
 static void
 repo_write_dispatch_session(int fd, short event, void *arg)
 {
@@ -2217,7 +2560,7 @@ repo_write_dispatch_session(int fd, short event, void *arg)
 	struct imsg imsg;
 	struct repo_write_client *client = &repo_write_client;
 	ssize_t n;
-	int shut = 0, have_packfile = 0;
+	int shut = 0;
 
 	if (event & EV_READ) {
 		if ((n = imsgbuf_read(ibuf)) == -1)
@@ -2274,18 +2617,42 @@ repo_write_dispatch_session(int fd, short event, void *arg)
 			err = protect_refs_from_deletion();
 			if (err)
 				break;
-			err = recv_packfile(&have_packfile, &imsg);
+			err = recv_packfile(&repo_write.have_packfile, &imsg);
 			if (err) {
 				log_warnx("receive packfile: %s", err->msg);
 				break;
 			}
-			if (have_packfile) {
+			if (repo_write.have_packfile) {
 				err = verify_packfile();
 				if (err) {
 					log_warnx("verify packfile: %s",
 					    err->msg);
 					break;
 				}
+			} else {
+				/*
+				 * Clients sending empty pack files might be
+				 * attempting to move a protected reference.
+				 */
+				err = protect_refs_from_moving();
+				if (err)
+					break;
+			}
+			err = send_packfile_received(iev,
+			    repo_write.have_packfile);
+			if (err)
+				log_warnx("receive packfile: %s", err->msg);
+			break;
+		case GOTD_IMSG_PACKFILE_GET_CONTENT:
+			if (!repo_write.have_packfile ||
+			    client->packidx == NULL) {
+				err = got_error(GOT_ERR_PRIVSEP_MSG);
+				break;
+			}
+			err = get_content_from_packfile(iev, &imsg);
+			break;
+		case GOTD_IMSG_PACKFILE_VERIFIED:
+			if (repo_write.have_packfile) {
 				err = install_packfile(iev);
 				if (err) {
 					log_warnx("install packfile: %s",
@@ -2298,14 +2665,6 @@ repo_write_dispatch_session(int fd, short event, void *arg)
 				 */
 				repo_write.repo->pack_path_mtime.tv_sec = 0;
 				repo_write.repo->pack_path_mtime.tv_nsec = 0;
-			} else {
-				/*
-				 * Clients sending empty pack files might be
-				 * attempting to move a protected reference.
-				 */
-				err = protect_refs_from_moving();
-				if (err)
-					break;
 			}
 			err = update_refs(iev);
 			if (err)
@@ -2434,7 +2793,7 @@ done:
 
 void
 repo_write_main(const char *title, const char *repo_path,
-    int *pack_fds, int *temp_fds,
+    int *pack_fds, int *temp_fds, FILE *base_file, FILE *accum_file,
     FILE *diff_f1, FILE *diff_f2, int diff_fd1, int diff_fd2,
     struct got_pathlist_head *protected_tag_namespaces,
     struct got_pathlist_head *protected_branch_namespaces,
@@ -2453,6 +2812,8 @@ repo_write_main(const char *title, const char *repo_path,
 	repo_write.pid = getpid();
 	repo_write.pack_fds = pack_fds;
 	repo_write.temp_fds = temp_fds;
+	repo_write.base_file = base_file;
+	repo_write.accum_file = accum_file;
 	repo_write.session_fd = -1;
 	repo_write.session_iev.ibuf.fd = -1;
 	repo_write.protected_tag_namespaces = protected_tag_namespaces;
@@ -2531,6 +2892,8 @@ repo_write_shutdown(void)
 		free(ref_update);
 	}
 
+	if (client->packidx)
+		got_packidx_close(client->packidx);
 	got_pack_close(&client->pack);
 	if (client->fd != -1)
 		close(client->fd);

gotd/repo_write.h
@@ -15,7 +15,7 @@
  */
 
 void repo_write_main(const char *, const char *, int *, int *,
-    FILE *, FILE *, int, int,
+    FILE *, FILE *, FILE *, FILE *, int, int,
     struct got_pathlist_head *, struct got_pathlist_head *,
     struct got_pathlist_head *);
 void repo_write_shutdown(void);

commit c944bea60e2705610f4f1669ea9ae2e651aa12b4

keep disconnecting gotd clients when important child processes exit
This should fix a regression introduced in
commit dda4473541d84973dd43a16f2f79ff0ce5f93dfe (make gotd run 'gotsys check'
on gotsys.conf commits before accepting them), where I removed a call to
the disconnect() function in proc_done(). We have now observed stale client
sessions accumulating on got.gameoftrees.org, blocking new connections from
anonymous users. This commit fixes the most likely reason for that issue.

gotd/gotd.c
@@ -300,6 +300,7 @@ static void
 proc_done(struct gotd_child_proc *proc)
 {
 	struct gotd_client *client;
+	int do_disconnect = 0;
 
 	TAILQ_REMOVE(&procs, proc, entry);
 
@@ -307,12 +308,18 @@ proc_done(struct gotd_child_proc *proc)
 	if (client == NULL)
 		client = find_client_by_proc_fd(proc->pipe[0]);
 	if (client != NULL) {
-		if (proc == client->repo)
+		if (proc == client->repo) {
 			client->repo = NULL;
-		if (proc == client->auth)
+			do_disconnect = 1;
+		}
+		if (proc == client->auth) {
 			client->auth = NULL;
-		if (proc == client->session)
+			do_disconnect = 1;
+		}
+		if (proc == client->session) {
 			client->session = NULL;
+			do_disconnect = 1;
+		}
 		if (proc == client->gotsys)
 			client->gotsys = NULL;
 	}
@@ -329,6 +336,9 @@ proc_done(struct gotd_child_proc *proc)
 	}
 
 	free(proc);
+
+	if (do_disconnect)
+		disconnect(client);
 }
 
 static void