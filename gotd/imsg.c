9619bad01228458bc938f90c107e32158e635bae
Stefan Sperling <stsp@stsp.name>
2025-03-24 15:38:31 +0000

    provide imsg-based variants of gotsys check/apply for gotd to use
    This allows for better inter-process communication, making error reporting
    easier. It also avoids closing standard output and standard error channels
    in a freshly forked child process, which seems to be a bad idea.

--- gotd/imsg.c
+++ gotd/imsg.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Stefan Sperling <stsp@openbsd.org>
+ * Copyright (c) 2022, 2025 Stefan Sperling <stsp@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
