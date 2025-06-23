c5e96df445562a55ab4860771be8f33067b8e74f
Stefan Sperling <stsp@stsp.name>
2025-06-23 13:20:22 +0000

    use imsgbuf_flush() instead of gotsysd_imsg_flush() in libexec event handlers
    The gotsysd_imsg_flush() wrapper enforces a sync write by polling the
    file descriptor and writing data in a loop. Since libevent already does
    polling for us we can simply use imsgbuf_flush() here instead.

--- gotsysd/libexec/gotsys-userkeys/gotsys-userkeys.c
+++ gotsysd/libexec/gotsys-userkeys/gotsys-userkeys.c
@@ -150,13 +150,10 @@ dispatch_event(int fd, short event, void *arg)
 	}
 
 	if (event & EV_WRITE) {
-		err = gotsysd_imsg_flush(ibuf);
-		if (err) {
-			warn("%s", err->msg);
+		if (imsgbuf_flush(ibuf) == -1) {
+			warn("imsgbuf_flush");
 			goto fatal;
-		}
-
-		if (imsgbuf_queuelen(ibuf) == 0 && flush_and_exit) {
+		} else if (imsgbuf_queuelen(ibuf) == 0 && flush_and_exit) {
 			event_del(&iev->ev);
 			event_loopexit(NULL);
 			return;
